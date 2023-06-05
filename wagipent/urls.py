292953dfaac4773cc0bf5aca29e72b3b0da32f02
bradiepoa <bradiepoa7@gmail.com>
2023-06-05 18:37:06 +0300

    changing blank fields to non

--- wagipent/urls.py
+++ wagipent/urls.py
@@ -28,7 +28,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('', include('mainweb.urls')),
-    path('admin/', admin.site.urls),
+    path('mgt/', admin.site.urls),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
